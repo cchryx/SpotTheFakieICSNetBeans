c8b84248a0942313cc5dba1056ed4ef922d2f531
Name69 <tangmm7799@gmail.com>
2025-05-20 10:38:26 -0400

    Create TrueFalse.java

---
+++ src/TrueFalse.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Stikb
+ */
+public class TrueFalse extends Question {
+    private boolean answer;
+    public TrueFalse(String name, boolean answer){
+        super(name);
+        this.answer = answer;
+    }
+    
+    
+}
